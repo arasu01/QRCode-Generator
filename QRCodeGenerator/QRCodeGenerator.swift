8e4bf08a9303404af0a58b2ac0dad58c19d8d81f
Arasuvel <knt1990@gmail.com>
2019-02-01 20:53:52 +0530

    added QRcode generator swift file.

---
+++ QRCodeGenerator/QRCodeGenerator.swift
@@ -0,0 +1,89 @@
+//
+//  QRCodeGenerator.swift
+//  QRCode
+//
+//  Created by Leo Chelliah on 25/01/19.
+//  Copyright © 2019 Leo Chelliah. All rights reserved.
+//
+
+import UIKit
+import CoreImage
+
+
+class QRCodeGenerator {
+
+    /// Creates a QR code for the current URL in the given color.
+    class func generateQRImage(string: String, using color: UIColor, logo: UIImage? = nil) -> CIImage? {
+        let tintedQRImage = QRCodeGenerator.generateQRImage(string: string)?.tinted(using: color)
+        
+        guard let logo = logo?.cgImage else {
+            return tintedQRImage
+        }
+        
+        return tintedQRImage?.combined(with: CIImage(cgImage: logo))
+    }
+    
+    /// Returns a black and white QR code for this URL.
+    class func generateQRImage(string: String) -> CIImage? {
+        guard let qrFilter = CIFilter(name: "CIQRCodeGenerator") else { return nil }
+        let qrData = string.data(using: .utf8)
+        qrFilter.setValue(qrData, forKey: "inputMessage")
+        
+        let qrTransform = CGAffineTransform(scaleX: 12, y: 12)
+        return qrFilter.outputImage?.transformed(by: qrTransform)
+    }
+    
+}
+
+extension CIImage {
+    /// Inverts the colors and creates a transparent image by converting the mask to alpha.
+    /// Input image should be black and white.
+    var transparent: CIImage? {
+        return inverted?.blackTransparent
+    }
+    
+    /// Inverts the colors.
+    var inverted: CIImage? {
+        guard let invertedColorFilter = CIFilter(name: "CIColorInvert") else { return nil }
+        
+        invertedColorFilter.setValue(self, forKey: "inputImage")
+        return invertedColorFilter.outputImage
+    }
+    
+    /// Converts all black to transparent.
+    var blackTransparent: CIImage? {
+        guard let blackTransparentFilter = CIFilter(name: "CIMaskToAlpha") else { return nil }
+        blackTransparentFilter.setValue(self, forKey: "inputImage")
+        return blackTransparentFilter.outputImage
+    }
+    
+    /// Applies the given color as a tint color.
+    func tinted(using color: UIColor) -> CIImage? {
+        guard
+            let transparentQRImage = transparent,
+            let filter = CIFilter(name: "CIMultiplyCompositing"),
+            let colorFilter = CIFilter(name: "CIConstantColorGenerator") else { return nil }
+        
+        let ciColor = CIColor(color: color)
+        colorFilter.setValue(ciColor, forKey: kCIInputColorKey)
+        let colorImage = colorFilter.outputImage
+        
+        filter.setValue(colorImage, forKey: kCIInputImageKey)
+        filter.setValue(transparentQRImage, forKey: kCIInputBackgroundImageKey)
+        
+        return filter.outputImage!
+    }
+}
+
+extension CIImage {
+    
+    /// Combines the current image with the given image centered.
+    func combined(with image: CIImage) -> CIImage? {
+        guard let combinedFilter = CIFilter(name: "CISourceOverCompositing") else { return nil }
+        let centerTransform = CGAffineTransform(translationX: extent.midX - (image.extent.size.width / 2), y: extent.midY - (image.extent.size.height / 2))
+        combinedFilter.setValue(image.transformed(by: centerTransform), forKey: "inputImage")
+        combinedFilter.setValue(self, forKey: "inputBackgroundImage")
+        return combinedFilter.outputImage!
+    }
+    
+}
